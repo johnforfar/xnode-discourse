d31ca674ecb2839dfb2bfc972dfc41833c7eded7
John Forfar <john.forfar@gmail.com>
2025-03-21 14:42:02 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,37 +2,37 @@
   description = "Minimal NixOS configuration for Discourse";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11"; # unstable for up to date software, 24.11 might be quite outdated
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-  in
-  {
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ({ config, pkgs, ... }: {
-          boot.isContainer = true;
-          systemd.services.discourse = {
-            description = "Discourse web application";
-            after = [ "network.target" ];
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-              ExecStart = "\${pkgs.discourse}/bin/rails server -b 0.0.0.0 -p 3000";
-              StateDirectory = "discourse";
-              DynamicUser = true;
-              Restart = "on-failure";
-            };
-          };
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          (
+            { ... }:
+            {
+              boot.isContainer = true;
 
-          networking.firewall.allowedTCPPorts = [ 3000 ];
+              services.discourse = {
+                enable = true;
+                hostname = "localhost";
+                enableACME = false;
+                database.ignorePostgresqlVersion = true;
+                admin.skipCreate = true;
+              };
 
-          system.stateVersion = "25.05";
+              networking.firewall.allowedTCPPorts = [ 80 ];
 
-        })
-      ];
+              system.stateVersion = "24.11";
+            }
+          )
+        ];
+      };
     };
-  };
 }
